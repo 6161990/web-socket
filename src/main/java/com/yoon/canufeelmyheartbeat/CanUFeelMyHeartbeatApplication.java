f04fbdef8ca822db915423ea7fae165b872c38be
6161990 <6161990src@gmail.com>
2025-02-06 23:46:31 +0900

    init project

---
+++ src/main/java/com/yoon/canufeelmyheartbeat/CanUFeelMyHeartbeatApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.canufeelmyheartbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanUFeelMyHeartbeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanUFeelMyHeartbeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoon/canufeelmyheartbeat/CanUFeelMyHeartbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.canufeelmyheartbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanUFeelMyHeartbeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
