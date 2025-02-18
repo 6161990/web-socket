ad68465f35a8c10779df3b0ab4ff808963bf63f3
6161990 <src6161990@i-nara.co.kr>
2025-02-18 23:52:47 +0900

    메세지 전송시 저장하기

--- src/main/java/com/yoon/canufeelmyheartbeat/controllers/StompChatController.java
+++ src/main/java/com/yoon/canufeelmyheartbeat/controllers/StompChatController.java
@@ -1,7 +1,9 @@
 package com.yoon.canufeelmyheartbeat.controllers;
 
 import com.yoon.canufeelmyheartbeat.dtos.ChatMessage;
-import com.yoon.canufeelmyheartbeat.services.DateRequestBroadcastor;
+import com.yoon.canufeelmyheartbeat.entities.Message;
+import com.yoon.canufeelmyheartbeat.services.ChatService;
+import com.yoon.canufeelmyheartbeat.vos.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -9,6 +11,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -17,12 +21,17 @@ import java.util.Map;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-public class StompController {
+public class StompChatController {
+
+    private final ChatService chatService;
 
     @MessageMapping("/chats/{chatroomId}") /* 어떤 경로로 publish 한 메세지를 라우팅할 것인지 매핑.  클라이언트는 /pub/chat 으로 메세지를 발행할거야 */
     @SendTo("/sub/chats/{chatroomId}") /* 클라이언트에게 return 되는 메세지를 보내줄 경로 */
     public ChatMessage handleMessage(@AuthenticationPrincipal Principal principal, @DestinationVariable Long chatroomId, @Payload Map<String, String> payload) {
         log.info("StompController handleMessage {} 메세지 도착함 {}, {}", principal.getName(), chatroomId, payload);
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User) ((OAuth2AuthenticationToken) principal).getPrincipal();
+
+        chatService.saveMessage(customOAuth2User.getMember(), chatroomId, payload.get("message"));
 
         return new ChatMessage(principal.getName(), payload.get("message"));
     }
